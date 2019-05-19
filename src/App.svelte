e81db85d39c2c65f42e65e0cb5da56bf143e3063
Cajotafer <hello@cajotafer.com>
2019-05-19 18:14:17 -0500

    UPDATE - binding tests

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  let value = 0;
+  function handleClick() {
+    value += 1;
+  }
+</script>
+
+<button on:click="{handleClick}">
+  Clicked {value} {value === 1 ? 'time' : 'times'}
+</button>
+<input type="number" bind:value />
